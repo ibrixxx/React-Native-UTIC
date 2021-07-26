11f54d20fe21304540bdd709ab060de25aedecbd
ibrahim <ibrahinmesan@gmail.com>
2021-07-26 14:03:06 +0200

    grades

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -1,18 +1,11 @@
-import {DrawerContentScrollView, DrawerItem, DrawerItemList} from "@react-navigation/drawer";
+import {DrawerContentScrollView, DrawerItemList} from "@react-navigation/drawer";
 import React from "react";
-import {Icon} from "react-native-elements";
 
 
 export default function CustomDrawerContent(props) {
     return (
         <DrawerContentScrollView {...props}>
             <DrawerItemList {...props} />
-            {/*<DrawerItem*/}
-            {/*    name="Close"*/}
-            {/*    label=''*/}
-            {/*    icon={() => <Icon name={'close'}/>}*/}
-            {/*    onPress={() => props.navigation.closeDrawer()}*/}
-            {/*/>*/}
         </DrawerContentScrollView>
     );
 }
