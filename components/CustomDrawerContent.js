e69f911fb8fbc1c0757a2a36d51460ef6a7fb1f2
bajramovic-asja00 <asja.bajramovic@gmail.com>
2021-08-02 15:03:55 +0200

    Drawer Menu Header

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -1,6 +1,9 @@
 import {DrawerContentScrollView, DrawerItemList} from "@react-navigation/drawer";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Text, View} from "react-native";
+import axios from "axios";
+import {TOKEN} from "../App";
+import {Avatar} from "react-native-elements";
 
 
 export default function CustomDrawerContent(props) {
@@ -9,20 +12,71 @@ export default function CustomDrawerContent(props) {
     //const newState = {...state};
     //newState.routes = newState.routes.filter(item => item.name !== 'AddPhone')
 
+    const [student, setStudent] = useState({});
+    const [studyProgram, setStudyProgram] = useState({});
+
+
+    useEffect(() => {
+        getUserData();
+        getStudyProgram();
+    }, [])
+
+    const getUserData = () => {
+        axios.get(' http://192.168.44.79:8080/u/0/students/student/personal-information', {
+            headers: {
+                Accept: 'application/json',
+                Authorization: TOKEN
+            }
+        })
+            .then(respnse => {
+                console.log(respnse.data)
+                setStudent(respnse.data)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    const getStudyProgram = () => {
+        axios.get('http://192.168.44.79:8080/u/0/students/student/personal-information/study-program', {
+            headers: {
+                Accept: 'application/json',
+                Authorization: TOKEN
+            }
+        })
+            .then(respnse => {
+                console.log(respnse.data)
+                setStudyProgram(respnse.data)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    function getInitials(){
+
+        return "MD"
+    }
+
     return (
         <DrawerContentScrollView {...props}>
             <View
                 style={{
-                    backgroundColor: '#f50057',
-                    height: 140,
+                    backgroundColor: '#434343',
+                    padding: 20,
                     alignItems: 'center',
-                    justifyContent: 'center',
+                    justifyContent: 'space-between',
+                    flexDirection: 'row'
                 }}
             >
-                <Text style={{ color: 'white', fontSize: 30 }}>
-                    Header
-                </Text>
+                <Avatar rounded title={getInitials()} size="large" overlayContainerStyle={{ backgroundColor: 'dodgerblue' }}/>
+                <View>
+                    <Text style={{ color: 'white', fontSize: 20 }}>{student.firstName} {student.lastName}</Text>
+                    <Text style={{ color: 'white' }}>index: {studyProgram.index}</Text>
+                </View>
+
             </View>
+
             <DrawerItemList {...props} />
 
         </DrawerContentScrollView>
