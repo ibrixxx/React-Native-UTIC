a919d2d0e788e033756bb292a1c8cda7c316fb00
ibrahim <ibrahinmesan@gmail.com>
2021-08-02 15:07:50 +0200

    rollback

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -1,6 +1,7 @@
 import {DrawerContentScrollView, DrawerItemList} from "@react-navigation/drawer";
 import React from "react";
 import {Text, View} from "react-native";
+import {Avatar, TouchableRipple} from 'react-native-paper';
 
 
 export default function CustomDrawerContent(props) {
@@ -11,18 +12,7 @@ export default function CustomDrawerContent(props) {
 
     return (
         <DrawerContentScrollView {...props}>
-            <View
-                style={{
-                    backgroundColor: '#f50057',
-                    height: 140,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                <Text style={{ color: 'white', fontSize: 30 }}>
-                    Header
-                </Text>
-            </View>
+
             <DrawerItemList {...props} />
 
         </DrawerContentScrollView>
